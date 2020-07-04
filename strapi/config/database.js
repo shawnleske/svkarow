4ed0d3a1b691dcfd64c2f096f3d352a6490a5abf
shawnleske <dev@shawnleske.me>
2020-07-04 03:04:44 +0200

    strapi on heroku

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -1,15 +1,15 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  "defaultConnection": "default",
+  "connections": {
+    "default": {
+      "connector": "mongoose",
+      "settings": {
+        "uri": "mongodb://shawnleske:wfSWvMcjYhUYW3k@svkarow-shard-00-00.bdyot.mongodb.net:27017,svkarow-shard-00-01.bdyot.mongodb.net:27017,svkarow-shard-00-02.bdyot.mongodb.net:27017/strapi?ssl=true&replicaSet=atlas-790n33-shard-0&authSource=admin&retryWrites=true&w=majority"
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+      "options": {
+        "ssl": true
+      }
+    }
+  }
+}
+);
--- strapi/extensions/users-permissions/config/jwt.js
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'f0dbb279-68f8-4fb8-a96e-0d09702410e2'
+  jwtSecret: process.env.JWT_SECRET || '9b10614b-6106-4a48-b0f8-d79c39e7154b'
 };
