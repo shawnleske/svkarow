f77caed2801e74ac6d2c5966ab4d88c510ddd689
shawnleske <dev@shawnleske.me>
2020-07-11 04:46:41 +0200

    added carousel, responsive design, officials, refs

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,67 @@ $(() => {
     initBanner();
     initMenu();
     initNews();
+    initSlick();
 });
+
+function initSlick() {
+    var $carousel = $('.carousel');
+
+    if(!$carousel.length)
+        return;
+
+    $carousel.each(function() {
+        var $carouselElems = $(this).children();
+
+        // TODO: BEWARE: Doesn't really work when children have different widths as it only checks the 1st elem
+        if($carouselElems.outerWidth(true)*$carouselElems.length >= $(this).parent().width()) {
+            createCarousel($(this));
+        }
+    });
+
+    $(window).resize(function() {
+        $carousel.each(function() {
+            var $this = $(this);
+            var $carouselElems = $this.hasClass('slick-initialized') ? $this.find('.slick-slide') : $this.children();
+
+            // TODO: BEWARE: Doesn't really work when children have different widths as it only checks the 1st elem
+            if($carouselElems.outerWidth(true)*$carouselElems.length >= $this.parent().width() && !$this.hasClass('slick-initialized')) {
+                createCarousel($this);
+            }
+            else if($carouselElems.outerWidth(true)*$carouselElems.length < $this.parent().width() && $this.hasClass('slick-initialized')) {
+                $this.slick('unslick');
+                $this.siblings('.carousel-movement').remove();
+            }
+        });
+    });
+}
+
+function createCarousel($this) {
+    var $carouselDots = $(document.createElement('div')).addClass('carousel-dots');
+    
+        var $carouselMovement = $(document.createElement('div'))
+            .addClass('carousel-movement')
+            .insertAfter($this)
+            .append($carouselDots);
+
+        $(document.createElement('div'))
+            .addClass('carousel-prev-arrow')
+            .insertBefore($carouselDots)
+
+        $(document.createElement('div'))
+                .addClass('carousel-next-arrow')
+                .insertAfter($carouselDots)
+
+        $this.slick({
+            infinite: false,
+            variableWidth: true,
+            centerMode: true,
+            dots: true,
+            prevArrow: $carouselMovement.find($('.carousel-prev-arrow')),
+            nextArrow: $carouselMovement.find($('.carousel-next-arrow')),
+            appendDots: $carouselMovement.find($('.carousel-dots'))
+        });
+}
 
 function initNews() {
     if(!$('.news').length)
